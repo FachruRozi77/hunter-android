f6530dd3f3067532891fde3c988702ec9bd4e74c
FachruRozi77 <fachru.r.dunggio@gmail.com>
2026-07-13 15:35:35 +0800

    Refactor Point.h and add SECP256K1.h

--- Point.h
+++ Point.h
@@ -1,4 +1,74 @@
-/*
+#ifndef SECP256K1H
+#define SECP256K1H
+
+#include "Point.h"
+#include <string>
+#include <vector>
+
+// Address type
+#define P2PKH 0
+#define P2SH 1
+#define BECH32 2
+
+class Secp256K1 {
+
+public:
+
+Secp256K1();
+~Secp256K1();
+void Init();
+Point ComputePublicKey(Int *privKey);
+
+// Batched version: computes `count` public keys at once with single batch inversion
+void ComputePublicKeyBatch(Int *privKeys, int count, Point *pubKeys);
+
+Point NextKey(Point &key);
+void Check();
+bool EC(Point &p);
+Int GetY(Int x, bool isEven);
+
+void GetHash160(int type,bool compressed,
+Point &k0, Point &k1, Point &k2, Point &k3,
+uint8_t *h0, uint8_t *h1, uint8_t *h2, uint8_t *h3);
+
+void GetHash160(int type,bool compressed, Point &pubKey, unsigned char *hash);
+
+std::string GetAddress(int type, bool compressed, Point &pubKey);
+std::string GetAddress(int type, bool compressed, unsigned char *hash160);
+std::vector<std::string> GetAddress(int type, bool compressed, unsigned char *h1, unsigned char *h2, unsigned char *h3, unsigned char *h4);
+std::string GetPrivAddress(bool compressed, Int &privKey );
+std::string GetPublicKeyHex(bool compressed, Point &p);
+Point ParsePublicKeyHex(std::string str, bool &isCompressed);
+
+bool CheckPudAddress(std::string address);
+
+static Int DecodePrivateKey(char *key,bool *compressed);
+
+Point Add(Point &p1, Point &p2);
+Point Add2(Point &p1, Point &p2);
+Point AddDirect(Point &p1, Point &p2);
+Point Double(Point &p);
+Point DoubleDirect(Point &p);
+
+Point G; // Generator
+Int order; // Curve order
+
+private:
+
+uint8_t GetByte(std::string &str,int idx);
+Point GTable[256*32]; // Generator table
+};
+
+#endif // SECP256K1H
+'''
+
+with open('/mnt/agents/output/SECP256K1.h', 'w') as f:
+    f.write(secp_h_content)
+
+print("SECP256K1.h written successfully")
+
+# Now create the remaining unchanged files
+point_h_content = r'''/*
  * This file is part of the BSGS distribution (https://github.com/JeanLucPons/BSGS).
  * Copyright (c) 2020 Jean Luc PONS.
  *
@@ -13,7 +83,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 #ifndef POINTH
 #define POINTH
@@ -24,22 +94,22 @@ class Point {
 
 public:
 
-  Point();
-  Point(Int *cx,Int *cy,Int *cz);
-  Point(Int *cx, Int *cz);
-  Point(const Point &p);
-  ~Point();
-  bool isZero();
-  bool equals(Point &p);
-  void Set(Point &p);
-  void Set(Int *cx, Int *cy,Int *cz);
-  void Clear();
-  void Reduce();
-  //std::string toString();
+Point();
+Point(Int *cx,Int *cy,Int *cz);
+Point(Int *cx, Int *cz);
+Point(const Point &p);
+~Point();
+bool isZero();
+bool equals(Point &p);
+void Set(Point &p);
+void Set(Int *cx, Int *cy,Int *cz);
+void Clear();
+void Reduce();
+//std::string toString();
 
-  Int x;
-  Int y;
-  Int z;
+Int x;
+Int y;
+Int z;
 
 };
 
