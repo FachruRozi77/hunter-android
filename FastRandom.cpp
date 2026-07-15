d0f8145a6d5de44d36f9a9e706288d50b500756d
FachruRozi77 <fachru.r.dunggio@gmail.com>
2026-07-15 12:58:55 +0800

    Refactor FastRandom implementation with consistent formatting

--- FastRandom.cpp
+++ FastRandom.cpp
@@ -18,7 +18,7 @@ inline uint64_t FastRandom::rotl(uint64_t x, int k) {
     return (x << k) | (x >> (64 - k));
 }
 
-inline uint64_t FastRandom::next() {
+uint64_t FastRandom::next() {
     const uint64_t result = rotl(s[1] * 5, 7) * 9;
     const uint64_t t = s[1] << 17;
     s[2] ^= s[0]; s[3] ^= s[1]; s[1] ^= s[2]; s[0] ^= s[3];
